b8ed039aab3c5361957fdd90efbf30b41dd11e27
Anton Lebedevich <mabrek@gmail.com>
2010-08-07 12:46:11 +0400

    unused deps removed from init-dependencies.sh script

--- init-dependencies.sh
+++ init-dependencies.sh
@@ -6,12 +6,4 @@ sudo apt-get install cdbs \
                      fakeroot \
                      quilt \
                      maven-repo-helper \
-                     ant \
-                     openjdk-6-jdk \
-                     libservlet2.5-java \
-                     libmx4j-java \
-                     libgnumail-java \
-                     libgnujaf-java \
-                     libcommons-daemon-java \
-                     libslf4j-java
-
+                     ant
