12e7a8d76d90105a7cc0147c18d1fb15b957cd73
Anton Lebedevich <mabrek@gmail.com>
2010-08-07 16:27:08 +0400

    forgot to call unpack

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ VERSION=$(dpkg-parsechangelog | egrep '^Version:' | cut -f2 -d' ' | sed 's/-[0-9
 DIR=jetty-$VERSION
 LOGFILE=`pwd`/build.log
 
+`dirname $0`/unpack.sh
+
 cd $DIR
 
 debuild -uc -us 2>&1 | tee $LOGFILE
